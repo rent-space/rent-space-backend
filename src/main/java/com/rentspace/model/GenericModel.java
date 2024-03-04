aca6a456d7327269b91cf3a989db75689218ec24
Vinícius Azevedo <sousa0240@gmail.com>
2024-03-04 11:06:58 -0300

    create PageRentSpace and adds comments to classes

--- src/main/java/com/rentspace/model/GenericModel.java
+++ src/main/java/com/rentspace/model/GenericModel.java
@@ -4,6 +4,11 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 
+/**
+ * This class will be inherited by all other entities in order to create a pattern to the Id implementation
+ *
+ * @author Vinícius Azevedo
+ */
 public abstract class GenericModel {
 
     @Id
---
+++ src/main/java/com/rentspace/util/PageRentSpace.java
@@ -0,0 +1,11 @@
+package com.rentspace.util;
+
+/**
+ * It would be interesting if we used a pagination logic to return elements, for example: 10 ou 100
+ * first results to the list of reservation requests, or similar. It is a more professional logic,
+ * and there are plenty of content online that is possible to take as inspiration
+ *
+ * @author Vinícius Azevedo
+ */
+public class PageRentSpace {
+}
