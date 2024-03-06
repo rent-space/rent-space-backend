f1f825ae4ede30d28452f486ca14993db04de67f
Vinícius Azevedo <sousa0240@gmail.com>
2024-03-05 23:14:41 -0300

    Update ModelMapperFuncs.java

--- src/main/java/com/rentspace/util/ModelMapperFuncs.java
+++ src/main/java/com/rentspace/util/ModelMapperFuncs.java
@@ -19,12 +19,4 @@ public abstract class ModelMapperFuncs {
 
     private ModelMapper modelMapper;
 
-    /*
-        TODO: Object translation funcions
-            1- itself (made automatically with modelMapper)
-            2- in pagination context
-            3- object to list
-            4- list to object
-     */
-
 }
