355872d13e0079213e556c004d286f46dc2e6387
Vinícius Azevedo <sousa0240@gmail.com>
2024-03-12 23:01:46 -0300

    Update UserRepository.java

--- src/main/java/com/rentspace/repository/UserRepository.java
+++ src/main/java/com/rentspace/repository/UserRepository.java
@@ -1,9 +1,8 @@
 package com.rentspace.repository;
 
-import com.rentspace.model.user.User;
+import com.rentspace.model.user.AppUser;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface UserRepository extends JpaRepository<User, Long> {
-}
+public interface UserRepository<T extends AppUser> extends JpaRepository<T, Long> {}
