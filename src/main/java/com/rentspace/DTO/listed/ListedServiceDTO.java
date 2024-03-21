a2ae5a24f91ec4ef92e55efbea44320cec018022
Vinícius Azevedo <sousa0240@gmail.com>
2024-03-21 09:58:29 -0300

    Create ListedServiceDTO.java

---
+++ src/main/java/com/rentspace/DTO/listed/ListedServiceDTO.java
@@ -0,0 +1,14 @@
+package com.rentspace.DTO.listed;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+public class ListedServiceDTO {
+
+    private Long id;
+    private String title;
+}
