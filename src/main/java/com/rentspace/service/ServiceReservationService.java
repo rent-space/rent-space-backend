0560c1a9854740e29a4f09a3bec664ab8d9c914e
Vinícius Azevedo <sousa0240@gmail.com>
2024-03-27 17:30:10 -0300

    Update ServiceReservationService.java

--- src/main/java/com/rentspace/service/ServiceReservationService.java
+++ src/main/java/com/rentspace/service/ServiceReservationService.java
@@ -3,15 +3,16 @@ package com.rentspace.service;
 import com.rentspace.DTO.persist.PersistServiceReservationDTO;
 import com.rentspace.DTO.response.ResponseServiceReservationDTO;
 import com.rentspace.repository.ServiceReservationRepository;
+import com.rentspace.util.ModelMapperFuncs;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
 @AllArgsConstructor
-public class ServiceReservationService {
+public class ServiceReservationService extends ModelMapperFuncs {
 
     private ServiceReservationRepository serviceReservationRepository;
-    
+
     public ResponseServiceReservationDTO create(PersistServiceReservationDTO persistDTO) {
         return null;
     }
