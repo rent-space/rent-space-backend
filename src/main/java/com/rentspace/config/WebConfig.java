0d7e19e4457856ef9f2ff8f9f91deb1221c1c994
Vinícius Azevedo <sousa0240@gmail.com>
2024-04-20 09:34:41 -0300

    inserts localhost and vercel domains

--- src/main/java/com/rentspace/config/WebConfig.java
+++ src/main/java/com/rentspace/config/WebConfig.java
@@ -12,7 +12,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("domínio da origem aqui");
+                .allowedOrigins("https://rent-space-br.vercel.app", "http://localhost");
     }
 }
 
