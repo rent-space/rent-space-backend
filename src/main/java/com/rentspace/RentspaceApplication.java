1419dfa9eefa279e15f274dba47321daf98e6cfe
Josias Batista <josias.leal@ccc.ufcg.edu.br>
2024-07-31 14:16:08 -0300

    Fixing issues for the deployment

--- src/main/java/com/rentspace/RentspaceApplication.java
+++ src/main/java/com/rentspace/RentspaceApplication.java
@@ -1,6 +1,5 @@
 package com.rentspace;
 
-import com.rentspace.initializer.FirebaseInitializer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -8,7 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class RentspaceApplication {
 
 	public static void main(String[] args) {
-		FirebaseInitializer.initialize();
 		SpringApplication.run(RentspaceApplication.class, args);
 	}
 }
